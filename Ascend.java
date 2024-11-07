495fb9be8d32d37cca6ccdac4e3d7fcc14f32666
shiramalamud <shira.malamud@post.runi.ac.il>
2024-11-07 22:44:34 +0200

    hw1

--- Ascend.java
+++ Ascend.java
@@ -1,7 +1,23 @@
 // Generates three integer random numbers in a given range,
 // and prints them in increasing order.
-public class Ascend {
-	public static void main(String[] args) {
-		// Replace this comment with your code
+public class Ascend 
+{
+	public static void main(String[] args) 
+	{
+		int lim=Integer.parseInt(args[0]);
+		int a= (int)(lim*(Math.random()));
+		int b= (int)(lim*(Math.random()));
+		int c= (int)(lim*(Math.random()));
+		System.out.println(a+" "+b+" "+c);
+		int min=Math.min(a,b);
+		int middle=Math.max(a,b);
+		int max=c;
+		max=Math.max(middle, c);
+		middle=Math.min(middle,c);
+		middle=Math.max(min,middle);
+		min=Math.min(min,c);
+		System.out.println(min+" "+middle+" "+max);
+		
+
 	}
 }
--- Bill3.java
+++ Bill3.java
@@ -3,6 +3,12 @@ public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
 	    String name1 = args[0];
-	    // Replace this comment with the rest of your code   
+		String name2 = args[1];
+		String name3 = args[2];
+	    int sum = Integer.parseInt(args[3]);
+		double seperate=sum/3.0;
+		seperate=Math.ceil(seperate);
+		System.out.println("Dear "+ name1 + ", " + name2 + ", and " + name3 + ": pay " + seperate + " Shekels each.");
+
 	}
 }
